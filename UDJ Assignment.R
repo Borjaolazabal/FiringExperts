c88fc626f6833155807ee34a3e9e093c9fd07168
m8rek <marek@zejgman.fr>
2015-10-06 18:12:48 +0800

    Add R script

---
+++ UDJ Assignment.R
@@ -0,0 +1,28 @@
+expertsdata= read.csv("S13FiringExperts.csv")
+
+# Start writing to an output file
+sink('udj-regression results.txt')
+
+regmodeljc=lm(US....Actual.~Jackie.Chan)
+summary(regmodeljc)
+
+regmodelb=lm(US....Actual.~Brad)
+summary(regmodelb)
+
+regmodels=lm(US....Actual.~Shahrukh)
+summary(regmodels)
+
+anova(regmodeljc, regmodelb,regmodels)
+
+regmodeljcb=lm(US....Actual.~Jackie.Chan + Brad)
+summary(regmodeljcb)
+
+regmodelbs=lm(US....Actual.~Brad + Shahrukh)
+summary(regmodelbs)
+
+regmodelsjc=lm(US....Actual.~Shahrukh + Jackie.Chan)
+summary(regmodelsjc)
+
+anova(regmodeljcb, regmodelbs,regmodelsjc)
+
+sink()
